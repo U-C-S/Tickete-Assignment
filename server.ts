c65256a279cc189cbe1cf6705810b97946aafb2f
U-C-S <uchanakyasrinivas@gmail.com>
2023-11-27 13:28:21 +0530

    fix host in prod

--- server.ts
+++ server.ts
@@ -1,11 +1,15 @@
 import { buildFastifyServer } from "./app";
+import "dotenv/config";
+
+const host = "RENDER" in process.env ? `0.0.0.0` : `localhost`;
+const port = parseInt(process.env.PORT || "4000");
 
 // IIFE
 (async () => {
   let x = await buildFastifyServer({
     logger: true,
   });
-  x.listen({ port: parseInt(process.env.PORT || "4000") }, (err, address) => {
+  x.listen({ host, port }, (err, address) => {
     if (err) {
       console.error(err);
     }
